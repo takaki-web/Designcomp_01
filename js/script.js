52d62acf1ea5a65cd5466ec69c3980b40a27b212
takaki-web <104483390+takaki-web@users.noreply.github.com>
2022-11-24 20:28:22 +0900

    ハンバーガー時背景固定追加

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 // ハンバーガーメニュー
 $(function () {
-  $('.js-l-header__burger-btn').click(function () {
-    $(this).toggleClass('active')
+  $(".js-l-header__burger-btn").click(function () {
+    $(this).toggleClass("active");
 
-    if ($(this).hasClass('active')) {
-      $('.js-l-header__burger-menu').addClass('active')
+    if ($(this).hasClass("active")) {
+      $(".js-l-header__burger-menu").addClass("active");
+      $("body").addClass("fixed");
     } else {
-      $('.js-l-header__burger-menu').removeClass('active')
+      $(".js-l-header__burger-menu").removeClass("active");
+      $("body").removeClass("fixed");
     }
-  })
-})
+  });
+});
 // ページ内リンクに飛ぶ時にリンククリックしたらハンバーガーメニューが閉じるように
-$('#menu a[href]').on('click', function (event) {
-  $('.js-l-header__burger-btn').trigger('click')
-})
-// ×ボタン押したらハンバーガーメニューが閉じるように
-$('.js-l-header__burger-menu').on('click', function (event) {
-  $('.js-l-header__burger-btn').trigger('click')
-})
-
-
+$(".l-header__burger-menu-link,a").on("click", function (event) {
+  $(".js-l-header__burger-btn").trigger("click");
+});
